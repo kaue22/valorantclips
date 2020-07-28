6fe3a2f2625e05b3c4423e09cfbc19cb6cc3e042
kaue <kauegouveia@gmail.com>
2020-07-28 17:35:18 -0300

    Alteações no footer e criação

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,13 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
-      <p>
-        Orgulhosamente criado durante a
+
+      
+        FEITOOO -
         {' '}
-        <a href="https://www.alura.com.br/">
+        
           Imersão React da Alura
-        </a>
-      </p>
+
     </FooterBase>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from './components/Button';
+import { Link } from 'react-router-dom'
 
-function Menu(){
-        return (
-            <nav className="Menu">
-                <a href="/">
+function Menu() {
+    return (
+        <nav className="Menu">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="ValorantClips Logo" />
-                </a>
+            </Link>
 
-                <Button as="a" className="ButtonLink" href="/">
-                    Novo Vídeo
-                </Button>
-            </nav>
-        );
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
 }
 
 export default Menu;
