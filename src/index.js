a772313bc6a57358a10bc41873ec15623ffa8559
kaue <kauegouveia@gmail.com>
2020-07-28 17:35:18 -0300

    Alteações no footer e criação
    A

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CadastroVideo from './pages/cadastro/video';
+import Home from './pages/Home/index';
+import {
+  Switch,
+  Route,
+  BrowserRouter
+} from "react-router-dom";
 
 
+//criar um jogo para pagina 404
+const Pagina404 = () => (<div>Essa Página não existe!!!</div>);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+
+    <Switch>
+      
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={Home} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
